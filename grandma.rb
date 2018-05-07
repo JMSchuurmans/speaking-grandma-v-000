63b4faa5d2a5ad9e30c07ce4b46f9d6d025caccc
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-07 16:12:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(conversation)
     "NO, NOT SINCE 1938!"
   elsif conversation.downcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif conversation == "I LOVE YOU GRANDMA!"
+  else conversation == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
