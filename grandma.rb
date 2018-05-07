337f26c4160179e271f130dada08738efc21702a
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-07 15:57:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -18,13 +18,8 @@ def speak_to_grandma(conversation)
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP SONNY!"
-  elsif 
+  elsif
     conversation = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
-end
-    
-  
-
-
 end
